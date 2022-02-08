f9538617049b6be31fd2acc86a2b6fc6623e108b
Iris-Smok <irissmok11@gmail.com>
2022-02-08 14:10:44 +0000

    fix bug for API throttling

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -80,6 +80,13 @@ function fetchGitHubInformation(event) {
       //if the promose don't work put we add an error() function that takes  an errorResponse
       if (errorResponse.status === 404) {
         $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
+      } else if (errorResponse.status === 403) {
+        var resetTime = new Date(
+          errorResponse.getResponseHeader("X-RateLimit-Reset") * 1000
+        );
+        $("#gh-user-data").html(
+          `<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`
+        );
       } else {
         console.log(errorResponse);
         $("#gh-user-data").html(
